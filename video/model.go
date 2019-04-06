4a494a19079f5aab4db8d6fea7a5388af79a60b5
0x113 <jose.guombinho@gmail.com>
2019-04-06 10:41:29 +0200

    changed movie model

--- video/model.go
+++ video/model.go
@@ -2,14 +2,13 @@ package video
 
 // Movie represtents model for movie
 type Movie struct {
-	MovieID     int64  `json:"movie_id"`
-	Title       string `json:"title"`
-	Description string `json:"description"`
-	Director    string `json:"director"`
-	Genre       string `json:"genre"`
-	Duration    string `json:"duration"`
-	Rate        string `json:"rate"`
-	ReleaseDate string `json:"release_date"`
-	FilePath    string `json:"file_path"`
-	PosterPath  string `json:"poster_path"`
+	MovieID     int64   `json:"movie_id"`
+	Title       string  `json:"title"`
+	Description string  `json:"description"`
+	Director    string  `json:"director"`
+	Genre       string  `json:"genre"`
+	Duration    string  `json:"duration"`
+	Rate        float64 `json:"rate"`
+	ReleaseDate string  `json:"release_date"`
+	PosterPath  string  `json:"poster_path"`
 }
