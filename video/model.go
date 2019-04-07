3c3a1fc5933db9dc33d98e27edbfe2a03ef13983
0x113 <jose.guombinho@gmail.com>
2019-04-07 13:20:40 +0200

    created model for tv series

--- video/model.go
+++ video/model.go
@@ -12,3 +12,15 @@ type Movie struct {
 	ReleaseDate string  `json:"release_date"`
 	PosterPath  string  `json:"poster_path"`
 }
+
+type TVSeries struct {
+	TvSeriesID      int64   `json:"tv_series_id"`
+	Title           string  `json:"title"`
+	Description     string  `json:"description"`
+	Director        string  `json:"director"`
+	Genre           string  `json:"genre"`
+	EpisodeDuration string  `json:"episode_duration"`
+	Rate            float64 `json:"rate"`
+	ReleaseDate     string  `json:"release_date"`
+	PosterPath      string  `json:"poster_path"`
+}
