68f6af3b4028dcdf7a4796d1c6cc40ad48a5ba1d
0x113 <jose.guombinho@gmail.com>
2019-04-09 14:41:45 +0200

    fixed jwt [key of invalid type]

--- auth/service.go
+++ auth/service.go
@@ -66,6 +66,6 @@ func (s *authService) getToken(user *User) (string, error) {
 	})
 
 	/* Sign the token with key */
-	return token.SignedString(env.EnvString("jwt_key"))
+	return token.SignedString([]byte(env.EnvString("jwt_key")))
 
 }
