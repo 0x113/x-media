1ce002301a06ca44d7333324b4e4a5ae36863b64
0x113 <jose.guombinho@gmail.com>
2019-08-12 14:31:42 +0200

    removed useless files

--- run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-env db_user="xmedia_user" db_pass="password" db_host="127.0.0.1" db_port="3306" db_name="xmedia" jwt_secret="secret" video_dir="/home/xa0s/Downloads/Movies/" movies_sub_dir="/home/xa0s/Downloads/Movies/sub" frontend_dir="./dist" ./x-media
-
