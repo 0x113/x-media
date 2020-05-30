1c7303a749b79e701a313a14ff73bc0a1ef81086
0x113 <jose.guombinho@gmail.com>
2020-05-30 13:59:22 +0000

    ObjectID instead of uint as TVShow id

--- tvshow/models/tvshow.go
+++ tvshow/models/tvshow.go
@@ -1,14 +1,16 @@
 package models
 
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
 // TVShow information
 type TVShow struct {
-	ID        uint     `bson:"_id" json:"id"`
-	Name      string   `bson:"name" json:"name"`
-	Language  string   `bson:"language" json:"language"`
-	Genres    []string `bson:"genres" json:"genres"`
-	Runtime   uint8    `bson:"runtime" json:"runtime"`
-	Premiered string   `bson:"premiered" json:"premiered"`
-	Rating    float32  `bson:"rating" json:"rating"`
-	PosterURL string   `bson:"poster_url" json:"poster_url"`
-	Summary   string   `bson:"summary" json:"summary"`
+	ID        primitive.ObjectID `bson:"_id" json:"id"`
+	Name      string             `bson:"name" json:"name"`
+	Language  string             `bson:"language" json:"language"`
+	Genres    []string           `bson:"genres" json:"genres"`
+	Runtime   uint8              `bson:"runtime" json:"runtime"`
+	Premiered string             `bson:"premiered" json:"premiered"`
+	Rating    float32            `bson:"rating" json:"rating"`
+	PosterURL string             `bson:"poster_url" json:"poster_url"`
+	Summary   string             `bson:"summary" json:"summary"`
 }
