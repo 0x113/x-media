477b9eead1746be8afb5bb016ddeae3a06f6d640
0x113 <jose.guombinho@gmail.com>
2020-05-30 19:13:33 +0000

    added struct validation

--- tvshow/models/tvshow.go
+++ tvshow/models/tvshow.go
@@ -4,13 +4,13 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 
 // TVShow information
 type TVShow struct {
-	ID        primitive.ObjectID `bson:"_id" json:"id"`
-	Name      string             `bson:"name" json:"name"`
-	Language  string             `bson:"language" json:"language"`
-	Genres    []string           `bson:"genres" json:"genres"`
-	Runtime   uint8              `bson:"runtime" json:"runtime"`
-	Premiered string             `bson:"premiered" json:"premiered"`
-	Rating    float32            `bson:"rating" json:"rating"`
-	PosterURL string             `bson:"poster_url" json:"poster_url"`
-	Summary   string             `bson:"summary" json:"summary"`
+	ID        primitive.ObjectID `bson:"_id" json:"id" validate:"omitempty"`
+	Name      string             `bson:"name" json:"name" validate:"required"`
+	Language  string             `bson:"language" json:"language" validate:"required"`
+	Genres    []string           `bson:"genres" json:"genres" validate:"required"`
+	Runtime   uint8              `bson:"runtime" json:"runtime" validate:"required"`
+	Premiered string             `bson:"premiered" json:"premiered" validate:"required"`
+	Rating    float32            `bson:"rating" json:"rating" validate:"required"`
+	PosterURL string             `bson:"poster_url" json:"poster_url" validate:"required,url"`
+	Summary   string             `bson:"summary" json:"summary" validate:"required"`
 }
