05467e98c2e5929df75a731113d36b263d89fde8
0x113 <jose.guombinho@gmail.com>
2020-06-01 14:50:08 +0000

    changed mock name

--- tvshow/mocks/inmem_repo.go
+++ tvshow/mocks/inmem_repo.go
@@ -6,19 +6,19 @@ import (
 	"github.com/0x113/x-media/tvshow/models"
 )
 
-// ITVShowRepo represents in-memory tv show repository
-type ITVShowRepo struct {
+// MockTVShowRepository represents in-memory tv show repository
+type MockTVShowRepository struct {
 	tvShows map[string]*models.TVShow
 }
 
-// NewInmemTVShowRepository creates new ITVShowRepo
-func NewInmemTVShowRepository() *ITVShowRepo {
+// NewInmemTVShowRepository creates new MockTVShowRepository
+func NewMockTVShowRepository() *MockTVShowRepository {
 	var tvShows = map[string]*models.TVShow{}
-	return &ITVShowRepo{tvShows}
+	return &MockTVShowRepository{tvShows}
 }
 
 // Save tv show in memory
-func (r *ITVShowRepo) Save(tvShow *models.TVShow) error {
+func (r *MockTVShowRepository) Save(tvShow *models.TVShow) error {
 	if _, ok := r.tvShows[tvShow.ID.Hex()]; ok {
 		return errors.New("TV Show already exists")
 	}
