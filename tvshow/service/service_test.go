2277da20d4d8033ef6c083ef59ab50b8ca5d1104
0x113 <jose.guombinho@gmail.com>
2020-06-01 14:05:45 +0000

    changed to test suite

--- tvshow/service/service_test.go
+++ tvshow/service/service_test.go
@@ -8,37 +8,64 @@ import (
 	"github.com/0x113/x-media/tvshow/models"
 	"github.com/0x113/x-media/tvshow/service"
 
-	log "github.com/sirupsen/logrus"
-	"github.com/stretchr/testify/assert"
+	"github.com/sirupsen/logrus"
+	"github.com/stretchr/testify/suite"
 )
 
-func TestSuccessSave(t *testing.T) {
-	tvShowRepo := mocks.NewInmemTVShowRepository()
-	tvShowService := service.NewTVShowService(tvShowRepo)
-	log.SetOutput(ioutil.Discard) // disable logrus for tests
-
-	tvShow := &models.TVShow{
-		Name:      "The Office",
-		Language:  "English",
-		Genres:    []string{"Comedy"},
-		Runtime:   30,
-		Premiered: "2005-03-24",
-		Rating:    8.5,
-		PosterURL: "https://static.tvmaze.com/uploads/images/original_untouched/85/213184.jpg",
-		Summary:   "Steve Carell stars in The Office, a fresh and funny mockumentary-style glimpse into the daily interactions of the eccentric workers at the Dunder Mifflin paper supply company. Based on the smash-hit British series of the same name and adapted for American Television by Greg Daniels, this fast-paced comedy parodies contemporary American water-cooler culture. Earnest but clueless regional manager Michael Scott believes himself to be an exceptional boss and mentor, but actually receives more eye-rolls than respect from his oddball staff.",
-	}
-
-	err := tvShowService.Save(tvShow)
-	assert.Nil(t, err)
+// TVShowServiceTestSuite define the suite
+type TVShowServiceTestSuite struct {
+	suite.Suite
+	tvShowRepo    *mocks.MockTVShowRepository
+	tvShowService service.TVShowService
 }
 
-func TestFailSave(t *testing.T) {
-	tvShowRepo := mocks.NewInmemTVShowRepository()
-	tvShowService := service.NewTVShowService(tvShowRepo)
-	log.SetOutput(ioutil.Discard) // disable logrus for tests
+// SetupTest initiates mocked database and new tv show service
+func (suite *TVShowServiceTestSuite) SetupTest() {
+	suite.tvShowRepo = mocks.NewMockTVShowRepository()
+	suite.tvShowService = service.NewTVShowService(suite.tvShowRepo)
+	logrus.SetOutput(ioutil.Discard) // disable logrus
+}
 
-	tvShow := &models.TVShow{}
+// TestTVShowServiceTestSuite runs test suite
+func TestTVShowServiceTestSuite(t *testing.T) {
+	suite.Run(t, new(TVShowServiceTestSuite))
+}
 
-	err := tvShowService.Save(tvShow)
-	assert.NotNil(t, err)
+func (suite *TVShowServiceTestSuite) TestSave() {
+	testCases := []struct {
+		name    string
+		tvShow  *models.TVShow
+		wantErr bool
+	}{
+		{
+			name: "Success",
+			tvShow: &models.TVShow{
+				Name:      "The Office",
+				Language:  "English",
+				Genres:    []string{"Comedy"},
+				Runtime:   30,
+				Premiered: "2005-03-24",
+				Rating:    8.5,
+				PosterURL: "https://static.tvmaze.com/uploads/images/original_untouched/85/213184.jpg",
+				Summary:   "One of the best tv shows ever",
+			},
+			wantErr: false,
+		},
+		{
+			name:    "Invalid struct",
+			tvShow:  &models.TVShow{},
+			wantErr: true,
+		},
+	}
+
+	for _, tt := range testCases {
+		suite.Run(tt.name, func() {
+			err := suite.tvShowService.Save(tt.tvShow)
+			if tt.wantErr {
+				suite.NotNil(err)
+			} else {
+				suite.Nil(err)
+			}
+		})
+	}
 }
