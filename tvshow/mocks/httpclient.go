20bf46ede68021b6747d10eb47b93472712fc4cc
0x113 <jose.guombinho@gmail.com>
2020-06-03 15:01:27 +0000

    added http client mock

---
+++ tvshow/mocks/httpclient.go
@@ -0,0 +1,15 @@
+package mocks
+
+import "net/http"
+
+// MockClient represents mocked http client
+type MockClient struct {
+	DoFunc func(req *http.Request) (*http.Response, error)
+}
+
+func (m *MockClient) Do(req *http.Request) (*http.Response, error) {
+	if m.DoFunc != nil {
+		return m.DoFunc(req)
+	}
+	return &http.Response{}, nil
+}
