b53db6aeebc6a94486b88adb15bd202e68db3834
0x113 <jose.guombinho@gmail.com>
2020-06-03 20:44:39 +0000

    added existing show and using Name as primary key

--- tvshow/mocks/inmem_repo.go
+++ tvshow/mocks/inmem_repo.go
@@ -14,14 +14,24 @@ type MockTVShowRepository struct {
 // NewInmemTVShowRepository creates new MockTVShowRepository
 func NewMockTVShowRepository() *MockTVShowRepository {
 	var tvShows = map[string]*models.TVShow{}
+	tvShows["BoJack Horseman"] = &models.TVShow{
+		Name:      "BoJack Horseman",
+		Language:  "English",
+		Genres:    []string{"Comedy", "Drama"},
+		Runtime:   25,
+		Premiered: "2014-08-22",
+		Rating:    8.1,
+		PosterURL: "https://static.tvmaze.com/uploads/images/original_untouched/236/590384.jpg",
+		Summary:   "Meet the most beloved sitcom horse of the '90s, 20 years later.",
+	}
 	return &MockTVShowRepository{tvShows}
 }
 
 // Save tv show in memory
 func (r *MockTVShowRepository) Save(tvShow *models.TVShow) error {
-	if _, ok := r.tvShows[tvShow.ID.Hex()]; ok {
+	if _, ok := r.tvShows[tvShow.Name]; ok {
 		return errors.New("TV Show already exists")
 	}
-	r.tvShows[tvShow.ID.Hex()] = tvShow
+	r.tvShows[tvShow.Name] = tvShow
 	return nil
 }
