e20d8e6b44f21f3d3499e88dec73902d3b88f2ea
0x113 <jose.guombinho@gmail.com>
2020-06-04 17:17:59 +0000

    added get tv show by name

--- tvshow/data/repository.go
+++ tvshow/data/repository.go
@@ -5,4 +5,5 @@ import "github.com/0x113/x-media/tvshow/models"
 // TVShowRepository contains all methods for operation og TVShow model
 type TVShowRepository interface {
 	Save(tvShow *models.TVShow) error
+	GetByName(name string) (*models.TVShow, error)
 }
