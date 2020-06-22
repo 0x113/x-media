775985b5c71ec6e25cb7dd43ade418f8a7ff7ec6
0x113 <jose.guombinho@gmail.com>
2020-06-22 17:22:28 +0000

    implemented user repository

---
+++ user/data/repository.go
@@ -0,0 +1,8 @@
+package data
+
+import "github.com/0x113/x-media/user/models"
+
+// UserRepository contains all methods for operation on User model
+type UserRepository interface {
+	Create(u *models.User) error
+}
