514f82bdc7b49b7f7c02668a1556d06ac2e8a2cc
0x113 <jose.guombinho@gmail.com>
2020-06-22 19:45:56 +0000

    changed mocks to store by username not by id

--- user/mocks/repository.go
+++ user/mocks/repository.go
@@ -8,13 +8,13 @@ import (
 
 // MockUserRepository representes in-memory user repository
 type MockUserRepository struct {
-	users map[int]*models.User
+	users map[string]*models.User
 }
 
 // NewMockUserRepository creates new instance of MockUserRepository
 func NewMockUserRepository() *MockUserRepository {
-	var users = map[int]*models.User{}
-	users[420] = &models.User{
+	var users = map[string]*models.User{}
+	users["JohnDoe"] = &models.User{
 		ID:       420,
 		Username: "JohnDoe",
 		Password: "ThisShouldSuperStrongAndSecret",
@@ -25,10 +25,10 @@ func NewMockUserRepository() *MockUserRepository {
 
 // Create new user in memory
 func (r *MockUserRepository) Create(u *models.User) error {
-	if _, ok := r.users[u.ID]; ok {
-		return fmt.Errorf("Couldn't create new user: user [id=%d] already exists", u.ID)
+	if _, ok := r.users[u.Username]; ok {
+		return fmt.Errorf("Couldn't create new user: user [username=%s] already exists", u.Username)
 	}
 
-	r.users[u.ID] = u
+	r.users[u.Username] = u
 	return nil
 }
