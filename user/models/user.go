c8865987b910c77650969a656e1ebf646e10304d
0x113 <jose.guombinho@gmail.com>
2020-06-22 16:58:57 +0000

    improved validation

--- user/models/user.go
+++ user/models/user.go
@@ -4,6 +4,6 @@ package models
 type User struct {
 	ID       int    `json:"id" validate:"omitempty"`
 	Username string `json:"username" validate:"required,min=3"`
-	Password string `json:"password" validate:"required,min=7"`
-	IsAdmin  bool   `json:"is_admin"`
+	Password string `json:"password" validate:"required,gte=8"`
+	IsAdmin  bool   `json:"is_admin" validate:"isdefault"`
 }
