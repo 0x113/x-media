9bb410ad1d447959d616534e991c693d6e57307b
0x113 <jose.guombinho@gmail.com>
2020-06-23 18:08:28 +0000

    initial sql

---
+++ user/init.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS user (
+  user_id int NOT NULL AUTO_INCREMENT,
+  username varchar(255) NOT NULL UNIQUE,
+  password varchar(60) NOT NULL,
+  is_admin BOOLEAN NOT NULL,
+  created_at DATETIME NOT NULL,
+  updated_at DATETIME NOT NULL,
+  PRIMARY KEY(user_id)
+);
+
