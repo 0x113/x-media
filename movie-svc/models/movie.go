c29a43cf1c14eca145829852dd3c71614d4cb4e3
0x113 <jose.guombinho@gmail.com>
2020-07-06 22:56:42 +0000

    added dir path to the user model

---
+++ movie-svc/models/movie.go
@@ -0,0 +1,18 @@
+package models
+
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
+// Movie defines the movie model
+type Movie struct {
+	ID               primitive.ObjectID `json:"id"`
+	Title            string             `json:"title"`
+	OriginalTitle    string             `json:"original_title"`
+	OriginalLanguage string             `json:"original_language"`
+	ReleaseDate      string             `json:"release_date"`
+	Genres           []string           `json:"genres"`
+	Rating           float32            `json:"rating"`
+	Runtime          int                `json:"runtime"`
+	BackdropPath     string             `json:"backdrop_path"`
+	PosterPath       string             `json:"poster_path"`
+	DirPath          string             `json:"dir_path"`
+}
