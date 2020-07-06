f5ceb79d8a7eb96c740fbc27db2b0edf27349ebe
0x113 <jose.guombinho@gmail.com>
2020-07-06 19:46:17 +0000

    removed old version

--- auth-old/model.go
+++
@@ -1,8 +0,0 @@
-package auth
-
-// User represents user model.
-type User struct {
-	ID       int64  `json:"-"`
-	Username string `json:"username"`
-	Password string `json:"password"`
-}
--- auth-old/repo.go
+++
@@ -1,7 +0,0 @@
-package auth
-
-// AuthRepository represtents authentication repository.
-type AuthRepository interface {
-	Create(user *User) error
-	GetUser(username string) (*User, error)
-}
