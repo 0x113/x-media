b4cff4a32a9c31db17d042b96dce2a81019329f4
0x113 <jose.guombinho@gmail.com>
2020-07-07 17:52:09 +0000

    added validation

--- movie-svc/models/movie.go
+++ movie-svc/models/movie.go
@@ -5,15 +5,15 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 // Movie defines the movie model
 type Movie struct {
 	ID               primitive.ObjectID `json:"id"`
-	TMDbID           int                `json:"tm_db_id"`
-	Title            string             `json:"title"`
-	OriginalTitle    string             `json:"original_title"`
-	OriginalLanguage string             `json:"original_language"`
-	ReleaseDate      string             `json:"release_date"`
-	Genres           []string           `json:"genres"`
-	Rating           float32            `json:"rating"`
-	Runtime          int                `json:"runtime"`
-	BackdropPath     string             `json:"backdrop_path"`
-	PosterPath       string             `json:"poster_path"`
-	DirPath          string             `json:"dir_path"`
+	TMDbID           int                `json:"tm_db_id" validate:"required"`
+	Title            string             `json:"title" validate:"required"`
+	OriginalTitle    string             `json:"original_title" validate:"required"`
+	OriginalLanguage string             `json:"original_language" validate:"required"`
+	ReleaseDate      string             `json:"release_date" validate:"required"`
+	Genres           []string           `json:"genres" validate:"required"`
+	Rating           float32            `json:"rating" validate:"required"`
+	Runtime          int                `json:"runtime" validate:"required"`
+	BackdropPath     string             `json:"backdrop_path" validate:"required"`
+	PosterPath       string             `json:"poster_path" validate:"required"`
+	DirPath          string             `json:"dir_path" validate:"required"`
 }
