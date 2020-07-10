7283378cd2264e10008a290a4c7e2e0449aa38b6
0x113 <jose.guombinho@gmail.com>
2020-07-10 17:05:53 +0000

    added bson

--- movie-svc/models/movie.go
+++ movie-svc/models/movie.go
@@ -4,19 +4,19 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 
 // Movie defines the movie model
 type Movie struct {
-	ID               primitive.ObjectID `json:"_id"`
-	TMDbID           int                `json:"tmdb_id" validate:"required"`
-	IMDbID           string             `json:"imdb_id"`
-	Title            string             `json:"title" validate:"required"`
-	Overview         string             `json:"overview" validate:"required"`
-	OriginalTitle    string             `json:"original_title" validate:"required"`
-	OriginalLanguage string             `json:"original_language" validate:"required"`
-	ReleaseDate      string             `json:"release_date" validate:"required"`
-	Genres           []string           `json:"genres" validate:"required"`
-	Rating           float32            `json:"rating" validate:"required"`
-	VoteCount        int                `json:"vote_count" validate:"required"`
-	Runtime          int                `json:"runtime"`
-	BackdropPath     string             `json:"backdrop_path" validate:"required"`
-	PosterPath       string             `json:"poster_path" validate:"required"`
-	DirPath          string             `json:"dir_path" validate:"required"`
+	ID               primitive.ObjectID `bson:"_id" json:"_id"`
+	TMDbID           int                `bson:"tmdb_id" json:"tmdb_id" validate:"required"`
+	IMDbID           string             `bson:"imdb_id" json:"imdb_id"`
+	Title            string             `bson:"title" json:"title" validate:"required"`
+	Overview         string             `bson:"overview" json:"overview" validate:"required"`
+	OriginalTitle    string             `bson:"original_title" json:"original_title" validate:"required"`
+	OriginalLanguage string             `bson:"original_language" json:"original_language" validate:"required"`
+	ReleaseDate      string             `bson:"release_date" json:"release_date" validate:"required"`
+	Genres           []string           `bson:"genres" json:"genres" validate:"required"`
+	Rating           float32            `bson:"rating" json:"rating" validate:"required"`
+	VoteCount        int                `bson:"vote_count" json:"vote_count" validate:"required"`
+	Runtime          int                `bson:"runtime" json:"runtime"`
+	BackdropPath     string             `bson:"backdrop_path" json:"backdrop_path" validate:"required"`
+	PosterPath       string             `bson:"poster_path" json:"poster_path" validate:"required"`
+	DirPath          string             `bson:"dir_path" json:"dir_path" validate:"required"`
 }
